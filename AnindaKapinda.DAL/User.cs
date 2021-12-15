26a89c2c18ea3279cd7c9f4fab3440f662586bc8
Emre Demirci <eemredemirci@gmail.com>
2021-12-16 02:27:07 +0300

    Added Entities and Controllers

---
+++ AnindaKapinda.DAL/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnindaKapinda.DAL
+{
+    class User
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Kullanıcı Adı gerekli")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Kullanıcı Soyadı gerekli")]
+        public string Surname { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Eposta gerekli")]
+
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage ="Şifre gerekli")]
+        public string Password { get; set; }
+
+    }
+}
---
+++ AuthenticationService/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+    }
+}
